875569f67ca0fca2a8582f11d53bab791e95fa32
donghyeok <donghyeok.jh@gmail.com>
2021-03-29 01:55:02 +0900

    add test code

---
+++ src/main/java/com/example/tdd1/DemoConfig.java
@@ -0,0 +1,22 @@
+package com.example.tdd1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class DemoConfig implements WebMvcConfigurer {
+
+    @Bean(name = "mvcTaskExecutor")
+    TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor te = new ThreadPoolTaskExecutor();
+        te.setCorePoolSize(10);
+        te.setMaxPoolSize(100);
+        te.setQueueCapacity(50);
+        te.initialize();
+        return te;
+    }
+}
--- src/main/java/com/example/tdd1/DemoDto2.java
+++ src/main/java/com/example/tdd1/DemoDto2.java
@@ -6,14 +6,14 @@ import lombok.*;
 @Setter
 @ToString
 @NoArgsConstructor
-public class DemoDto {
+public class DemoDto2 {
     private String userId;
     private String userPassword;
     private String userName;
     private int age;
 
     @Builder
-    public DemoDto(String userId, String userPassword, String userName, int age) {
+    public DemoDto2(String userId, String userPassword, String userName, int age) {
         this.userId = userId;
         this.userPassword = userPassword;
         this.userName = userName;
